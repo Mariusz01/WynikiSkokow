a79f9027c5b8e4e0f53718adb3dd2fc573a0d2dc
Mariusz01 <45500049+Mariusz01@users.noreply.github.com>
2020-05-11 12:41:25 +0200

    Trzeba się zalogować aby coś dodać, usunąć lub edytować Proste tworzenie konta

---
+++ WynikiSkokow/dbo.Skoczek.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Skoczek] (
+    [ID]       INT             IDENTITY (1, 1) NOT NULL,
+    [Imie]     NVARCHAR (MAX)  NOT NULL,
+    [Nazwisko] NVARCHAR (MAX)  NOT NULL,
+    [Kraj]     NVARCHAR (MAX)  NOT NULL,
+    [Skok1]    DECIMAL (18, 2) NULL,
+    [Skok2]    DECIMAL (18, 2) NULL,
+    [Punkty]   DECIMAL (18, 2) NULL,
+    [Miejsce]  INT             NULL,
+    CONSTRAINT [PK_Skoczek] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
